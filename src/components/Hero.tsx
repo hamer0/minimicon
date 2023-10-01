fd25d9d93946f7a70bb079929d8b973d655a4a46
Lewis <lewishamer0@gmail.com>
2023-10-01 16:05:16 +0100

    change hero text, lighten radio group hover colour

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ interface HeroProps {
 const Hero = ({iconCount}:HeroProps) => {
     return (
       <hgroup className="hero">
-        <h1>Clean and minimal SVG icons for your UI</h1>
+        <h1>minimal SVG icons for your UI</h1>
         <h2>{iconCount && `${iconCount} Icons · `}MIT License · 100% Free forever</h2>
       </hgroup>
     )
