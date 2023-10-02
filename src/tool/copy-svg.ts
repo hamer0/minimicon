a573a2ff8bcbab921df61691a6e147e86a621e0c
Lewis <lewishamer0@gmail.com>
2023-10-02 17:41:52 +0100

    implemented SVG reformat functions for JSX & RNSVG

--- src/tool/copy-svg.ts
+++ src/tool/copy-svg.ts
@@ -1,11 +1,26 @@
-export const svgToJSX = (svg: string) => {
-    console.log("convert to jsx");
-    return svg;
+const attributesToCamelCase = (str: string) => str.replace(/([-][a-z])/g, (match) => {
+    return match.substring(1).toUpperCase(); // remove leading dash and capitalise the letter
+});
+
+const capitaliseTags = (str: string) => {
+    const tagRegex = /<\s*\/?\s*([a-zA-Z]+)/g;
+
+    return str.replace(tagRegex, (match, tagName) => {
+        const capitalizedTagName = tagName.charAt(0).toUpperCase() + tagName.slice(1); //capitalise first letter
+        const isClosingTag = match.charAt(1) === '/';
+
+        return `<${isClosingTag ? '/' : ''}${capitalizedTagName}`;
+    });
+}
+
+export const svgToJSX = (svg: string) => {;
+    const jsx = attributesToCamelCase(svg);
+    return jsx;
 }
 
 export const jsxToRNSVG = (jsx: string) => {
-    console.log("convert to rnsvg");
-    return jsx;
+    const rnsvg = capitaliseTags(jsx);
+    return rnsvg;
 }
 
 export const svgToRNSVG = (svg: string) => jsxToRNSVG(svgToJSX(svg));
@@ -16,8 +31,6 @@ export const svgFormats: SVGCopyFormats = {
     "JSX": svgToJSX,
     "React Native SVG": svgToRNSVG,
 }
-
-//
 
 const copyToClipboard = (value: string) => {
     navigator.clipboard.writeText(value);
